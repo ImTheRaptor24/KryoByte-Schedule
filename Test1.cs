61466305fe878ecb2faa471b8ab00bcd48282982
ImTheRaptor24 <102767000+ImTheRaptor24@users.noreply.github.com>
2022-04-20 19:45:43 -0500

    Add files via upload

---
+++ Test1.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp5
+{
+    public class Test1
+    {
+        [Required]
+        [StringLength(80, ErrorMessage = "Name is too long.")]
+        public string? Name { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Phone number is too long.")]
+        public string? Phone { get; set; }
+
+        [Required]
+        [StringLength(80, ErrorMessage = "Email is too long.")]
+        public string? Email { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "Machine Make is too long.")]
+        public string? MachineMake { get; set; }
+
+        [Required]
+        [StringLength(80, ErrorMessage = "Machine Model is too long.")]
+        public string? MachineModel { get; set; }
+
+
+    }
+}
